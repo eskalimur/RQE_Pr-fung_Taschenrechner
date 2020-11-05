3a78a1fa54f8a86162551b655d342c781d2c6e46
Franz <wi19b084@technikum-wien.at>
2020-11-05 09:52:53 +0100

    Projektdateien hinzufügen.

---
+++ RQE_Prüfung_Taschenrechner/SpecFlowFeature1.feature
@@ -0,0 +1,33 @@
+﻿Feature: Trigonometrischer Taschenrechner
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the trigonometric results of a number
+
+@mytag
+Scenario: Calculate Sinus
+	Given a <number>
+	When i want to know the Sinus
+	Then the result should be <result>
+
+Examples: 
+| number  | result |
+| PI	  | 0      |
+
+Scenario Outline: Calculate Cosinus
+	Given a <number>
+	When i want to know the Cosinus
+	Then the result should be <result>
+
+Examples: 
+| number  | result |
+| PI	  | -1     |
+
+
+Scenario Outline: Calculate Tangens
+	Given a <number>
+	When i want to know the Tangens
+	Then the result should be <result>
+
+Examples: 
+| number  | result |
+| PI	  | 0      |
---
+++ RQE_Prüfung_Taschenrechner/TrigonometrischerTaschenrechnerSteps.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace RQE_Prüfung_Taschenrechner
+{
+    [Binding]
+    public class TrigonometrischerTaschenrechnerSteps
+    {
+        double number, sum;
+
+        [Given(@"a (.*)")]
+        public void GivenA(string p0)
+        {
+            if (p0 == "PI")
+                this.number = Math.PI;
+            else
+                this.number = 0;
+        }
+
+        [When(@"i want to know the Sinus")]
+        public void WhenIWantToKnowTheSinus()
+        {
+            sum = Math.Sin(this.number);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            double expected = p0;
+            double actual = this.sum;
+            Assert.Equal(expected, actual, 5);
+        }
+
+        [When(@"i want to know the Cosinus")]
+        public void WhenIWantToKnowTheCosinus()
+        {
+            sum = Math.Cos(this.number);
+        }
+
+        [When(@"i want to know the Tangens")]
+        public void WhenIWantToKnowTheTangens()
+        {
+            sum = Math.Tan(this.number);
+        }
+
+    }
+}
